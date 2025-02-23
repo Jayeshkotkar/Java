9cbbe88dd7fe324f60340312f1a8e275467c3060
Jayesh Kotkar <117332855+Jayeshkotkar@users.noreply.github.com>
2025-02-23 23:29:10 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,24 @@
+/* accept limit from user and display array */
+import java.util.*;
+class a1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter array limit=");
+        int n=sc.nextInt();
+
+        int a[]=new int[n];
+
+        System.out.println("enter array element=");
+        for(int i=0; i<a.length; i++)
+        {
+            a[i]=sc.nextInt();
+        }
+
+        System.out.println("array=");
+        for(int i=0; i<a.length; i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
